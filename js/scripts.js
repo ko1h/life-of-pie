d2fe105dc05763c0dc69d2654091fd817fa6cc78
kohichi horii <kohichi.horii@gmail.com>
2019-02-04 18:13:29 -0800

    Add vaule to each protein and starting price of the pizza to 5

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@
 function Pizza(size, protein, topping) {
   this.size = size,
   this.protein = protein,
-  this.topping = topping,
+  this.topping = topping
+  this.startPrice = 5,
+  this.toppingPrice = 0,
+  this.proteinPrice = 2
 }
 
 function add(size, protein, topping) {
@@ -18,6 +21,15 @@ Pizza.prototype.cost = function() {
   this.startPrice += add(this.size, this.proteinPrice, this.toppingPrice);
 }
 
+Pizza.prototype.outputPizzaInfo = function() {
+  $(".totalCost").text("Your total cost: " + "$" + this.startPrice);
+  this.topping.forEach(function(topping) {
+    $("#toppingsList").append("<li>" + topping + "</li>");
+  })
+  this.protein.forEach(function(protein) {
+    $("#proteinList").append("<li>" + protein + "</li>");
+  })
+}
 //user Intergface Logic
 $(document).ready(function() {
   $("#pizzaOrder").submit(function(event) {
